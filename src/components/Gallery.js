2dfdbe5b3f95e897eeb391cc0306b7bbea95176a
Lester Yiu <lester.ws.yiu@gmail.com>
2022-07-21 20:02:53 -0400

    added home page styles

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,26 @@
+import axios from "axios";
+
 const Gallery = () => {
+
+    // axios({
+    //     url: 'https://api.spoonacular.com/recipes/complexSearch',
+    //     method: 'GET',
+    //     dataResponse: 'json',
+    //     params: {
+    //         apiKey: 'b816c6f070174a9596e8c0889839e0da',
+    //         instructionsRequired: true,
+    //         addRecipeInformation: true
+    //     }
+    // }).then( (response) => {
+    //     console.log(response.data.results);
+    // })
+
     return(
-        <section></section>
+        <section className="gallerySection">
+            <div className="wrapper">
+                <h1>What type of cuisines are you craving for?</h1>
+            </div>
+        </section>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import Gallery from './Gallery';
+import fruitBowl from '../assets/fruit-bowl-2-img.webp';
+import fruitBowlTwo from '../assets/fruit-bowl-img.webp';
 
 const Home = () => {
     return(
@@ -7,6 +9,12 @@ const Home = () => {
                 <button className="searchButton"><i className="fa-solid fa-magnifying-glass"></i>Search for Recipes</button>
             </section>
             <Gallery/>
+            <div className="bowlImage">
+                <img src={fruitBowl} alt="" />
+            </div>
+            <div className="bowlImageTwo">
+                <img src={fruitBowlTwo} alt="" />
+            </div>
         </main>
     )
 }
