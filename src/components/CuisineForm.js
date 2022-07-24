1b5029027699451c1ab24812eb070350197c9da5
Lester Yiu <lester.ws.yiu@gmail.com>
2022-07-24 16:39:48 -0400

    added images for all cuisines, created filtering functionality

---
+++ src/components/CuisineForm.js
@@ -0,0 +1,168 @@
+import Carousel from 'react-elastic-carousel';
+import { useState } from 'react';
+
+const CuisineForm = (props) => {
+
+    const {breakPoints, setUserCuisine, userCuisine} = props;
+
+    const [isAfricanChecked, setIsAfricanChecked] = useState(false);
+    const [isAmericanChecked, setIsAmericanChecked] = useState(false);
+    const [isBritishChecked, setIsBritishChecked] = useState(false);
+    const [isCajunChecked, setIsCajunChecked] = useState(false);
+    const [isCarribeanChecked, setIsCarribeanChecked] = useState(false);
+    const [isChineseChecked, setIsChineseChecked] = useState(false);
+
+    const handleClick = (e, setCheckedFunc) => {
+
+        if (e.target.checked === false) {
+            let cuisineChoice = [...userCuisine];
+            setUserCuisine(cuisineChoice.filter( (i) => {
+                return i !== e.target.value;
+            }));
+            setCheckedFunc(false);
+        } else {
+            setUserCuisine([e.target.value , ...userCuisine]);
+            setCheckedFunc(true);
+        }
+    }
+
+    return(
+        <form action="">
+            <legend><h1>What type of cuisines are you craving for?</h1></legend>
+            <Carousel focusOnSelect={true} breakPoints={breakPoints}>
+                <div className="foodContainer">
+                    <label htmlFor="african" className={`african choice ${isAfricanChecked ? 'checked' : 'unchecked'}`}><span className='sr-only'>African</span></label>
+                    <input id="african" type="checkbox" name="cuisine" value="african" className="sr-only" onClick={(e) => handleClick(e, setIsAfricanChecked)} />
+                    <p className="cuisineIdentifier" aria-hidden="true">African</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="american" className="american choice"><span className="sr-only">American</span></label>
+                    <input id="american" type="checkbox" name="cuisine" value="american" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">American</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="british" className="british choice"><span className="sr-only">British</span></label>
+                    <input id="british" type="checkbox" name="cuisine" value="british" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">British</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="cajun" className="cajun choice"><span className="sr-only">Cajun</span></label>
+                    <input id="cajun" type="checkbox" name="cuisine" value="cajun" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Cajun</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="caribbean" className="carribean choice"><span className="sr-only">Carribean</span></label>
+                    <input id="caribbean" type="checkbox" name="cuisine" value="caribbean" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Carribean</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="chinese" className="chinese choice"><span className="sr-only">Chinese</span></label>
+                    <input id="chinese" type="checkbox" name="cuisine" value="chinese" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Chinese</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="eastern european" className="easternEuropean choice"><span className="sr-only">Eastern European</span></label>
+                    <input id="eastern european" type="checkbox" name="cuisine" value="eastern european" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Eastern European</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="european" className="european choice"><span className="sr-only">European</span></label>
+                    <input id="european" type="checkbox" name="cuisine" value="european" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">European</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="french" className="french choice"><span className="sr-only">French</span></label>
+                    <input id="french" type="checkbox" name="cuisine" value="french" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">French</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="german" className="german choice"><span className="sr-only">German</span></label>
+                    <input id="german" type="checkbox" name="cuisine" value="german" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">German</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="greek" className="greek choice"><span className="sr-only">Greek</span></label>
+                    <input id="greek" type="checkbox" name="cuisine" value="greek" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Greek</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="indian" className="indian choice"><span className="sr-only">Indian</span></label>
+                    <input id="indian" type="checkbox" name="cuisine" value="indian" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Indian</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="irish" className="irish choice"><span className="sr-only">Irish</span></label>
+                    <input id="irish" type="checkbox" name="cuisine" value="irish" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Irish</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="italian" className="italian choice"><span className="sr-only">Italian</span></label>
+                    <input id="italian" type="checkbox" name="cuisine" value="italian" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Italian</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="japanese" className="japanese choice"><span className="sr-only">Japanese</span></label>
+                    <input id="japanese" type="checkbox" name="cuisine" value="japanese" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Japanese</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="jewish" className="jewish choice"><span className="sr-only">Jewish</span></label>
+                    <input id="jewish" type="checkbox" name="cuisine" value="jewish" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Jewish</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="korean" className="korean choice"><span className="sr-only">Korean</span></label>
+                    <input id="korean" type="checkbox" name="cuisine" value="korean" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Korean</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="latin american" className="latinAmerican choice"><span className="sr-only">Latin American</span></label>
+                    <input id="latin american" type="checkbox" name="cuisine" value="latin american" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Latin American</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="mediterranean" className="mediterranean choice"><span className="sr-only">Mediterranean</span></label>
+                    <input id="mediterranean" type="checkbox" name="cuisine" value="mediterranean" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Mediterranean</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="mexican" className="mexican choice"><span className="sr-only">Mexican</span></label>
+                    <input id="mexican" type="checkbox" name="cuisine" value="mexican" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Mexican</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="middle eastern" className="middleEastern choice"><span className="sr-only">Middle Eastern</span></label>
+                    <input id="middle eastern" type="checkbox" name="cuisine" value="middle eastern" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Middle Eastern</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="nordic" className="nordic choice"><span className="sr-only">Nordic</span></label>
+                    <input id="nordic" type="checkbox" name="cuisine" value="nordic" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Nordic</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="southern" className="southern choice"><span className="sr-only">Southern</span></label>
+                    <input id="southern" type="checkbox" name="cuisine" value="southern" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Southern</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="spanish" className="spanish choice"><span className="sr-only">Spanish</span></label>
+                    <input id="spanish" type="checkbox" name="cuisine" value="spanish" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Spanish</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="thai" className="thai choice"><span className="sr-only">Thai</span></label>
+                    <input id="thai" type="checkbox" name="cuisine" value="thai" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Thai</p>
+                </div>
+                <div className="foodContainer">
+                    <label htmlFor="vietnamese" className="vietnamese choice"><span className="sr-only">Vietnamese</span></label>
+                    <input id="vietnamese" type="checkbox" name="cuisine" value="vietnamese" className="sr-only" onClick={handleClick}/>
+                    <p className="cuisineIdentifier" aria-hidden="true">Vietnamese</p>
+                </div>
+            </Carousel>
+        </form>
+    )
+}
+
+export default CuisineForm;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,6 @@
-import axios from "axios";
 import Form from "./Form";
 
 const Gallery = () => {
-
-    // axios({
-    //     url: 'https://api.spoonacular.com/recipes/complexSearch',
-    //     method: 'GET',
-    //     dataResponse: 'json',
-    //     params: {
-    //         apiKey: 'b816c6f070174a9596e8c0889839e0da',
-    //         instructionsRequired: true,
-    //         addRecipeInformation: true
-    //     }
-    // }).then( (response) => {
-    //     console.log(response.data.results);
-    // })
 
     return(
         <section className="gallerySection">
